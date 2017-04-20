3594c3774aa7b8d2e185a17e46cbd10e2f929e3c
keerthana <keerthukrishna70@gmail.com>
2017-04-20 17:59:26 +0530

    first commit

---
+++ VideoTube/app/src/main/java/com/example/akeer/videotube/Config.java
@@ -0,0 +1,14 @@
+package com.example.akeer.videotube;
+
+/**
+ * Created by akeer on 4/20/2017.
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyBqbitOOoeFtJHVr9DSnp1ID6v_1oPzD9g";
+
+}
